fa4521dad8b06b6d3ec6ca2579ac33b49b9e904e
nikeisme <nikeisme827@gmail.com>
2022-09-12 21:26:19 +0900

    :construction: Add: 상품 모델링

--- products/models.py
+++ products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+from commons.models import TimeStampedModel
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(TimeStampedModel):
+
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    stock = models.IntegerField(default=1)
+    description = models.TextField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False, blank=False)
+
+    class Meta:
+        db_table = "products"
+
+        def __str__(self):
+            return self.name
+
+
+class ProductImage(TimeStampedModel):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False, related_name="image")
+    image = models.ImageField(upload_to='profile/',default='default.png')
+
+    class Meta:
+        db_table = "products_images"
+
+    def __str__(self):
+        return self.name
--- shoppingmall/settings.py
+++ shoppingmall/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
-    'users'
+    'users',
+    'products',
 ]
 
 MIDDLEWARE = [
