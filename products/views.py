da2e6b42fc29d9e45ceef6514cd45c69985e6650
nikeisme <nikeisme827@gmail.com>
2022-09-14 20:29:53 +0900

    :sparkles: Add : 관리자만 프로덕트 CRUD 권한 부여- isAdminuser

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 # from core.permissions import IsAdminOrReadOnly
 
@@ -10,4 +11,10 @@ from products.serializers import ProductSerializer
 class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    # 사용자 인증
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    permission_classes = [IsAdminUser] # is_staff = True 인 계정에만 권한 부여
 
