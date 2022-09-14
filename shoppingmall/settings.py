f222cfd773c4182339774ae7facae57d10add119
nikeisme <nikeisme827@gmail.com>
2022-09-15 00:16:36 +0900

    :sparkles: Add : 결제 앱 crud 기능 구현 및 권한 부여

--- shoppingmall/settings.py
+++ shoppingmall/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "users",
     "products",
     "orders",
+    "payments"
 ]
 
 MIDDLEWARE = [
--- shoppingmall/urls.py
+++ shoppingmall/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include('users.urls')),
     path("products/", include('products.urls')),
-    path("orders/",include('orders.urls'))
+    path("orders/",include('orders.urls')),
+    path("payments/",include('payments.urls'))
 ]
