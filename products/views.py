9ef2c45870728c5c34373114e61af59299b87687
nikeisme <nikeisme827@gmail.com>
2022-09-14 22:12:48 +0900

    :pencil2: Fix : 프로덕트 cud 권한 관리자에게 부여, 이용자는 r만 가능

--- products/views.py
+++ products/views.py
@@ -5,16 +5,22 @@ from rest_framework.permissions import IsAdminUser
 # from core.permissions import IsAdminOrReadOnly
 
 from products.models import Product
+from products.permissions import IsSellerOrReadOnly
 from products.serializers import ProductSerializer
 
 
 class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    permission_classes = [IsSellerOrReadOnly]  # is_staff = True 인 계정에만 권한 부여
+
+    def get_serializer_class(self):
+        if self.action == 'list' or 'retrieve':
+            return ProductSerializer
+        return ProductSerializer
 
     # 사용자 인증
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        serializer.save(seller=self.request.user)
 
-    permission_classes = [IsAdminUser] # is_staff = True 인 계정에만 권한 부여
+
 
--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class CustomRegisterSerializer(RegisterSerializer):
     # 기본 설정 필드: username, password, email
-    # 추가 설정 필드: profile_image
+    # 추가 설정 필드: is_staff
     is_staff = serializers.BooleanField(allow_null=True)
 
     def get_cleaned_data(self):
